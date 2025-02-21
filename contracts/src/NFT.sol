0142aa2f6b663510632ec81bfdf34de056527bae
Dyslex7c <polynsominiac@gmail.com>
2025-02-21 12:26:35 +0530

    refactor: nft contracts

--- contracts/src/NFT.sol
+++ contracts/src/NFT.sol
@@ -1,90 +1,117 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.26;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT {
-    uint256 private _tokenIds;
-    address contractAddress;
-
-    // Enum for NFT tiers
-    enum Tier { COMMON, RARE, EPIC, LEGENDARY }
-
-    // Struct to store NFT metadata
-    struct NFTMetadata {
-        string name;
-        uint256 weight;
-        Tier tier;
-        string ipfsHash;
-    }
-
-    // Mapping from tokenId to metadata
-    mapping(uint256 => NFTMetadata) private _tokenMetadata;
-
+contract NFT is ERC721URIStorage, ReentrancyGuard, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    // Marketplace contract address
+    address private immutable _marketplaceAddress;
+    
+    // Mapping to track if a token URI has been used
+    mapping(string => bool) private _usedTokenURIs;
+    
     // Events
     event NFTMinted(
         uint256 indexed tokenId,
-        string name,
-        uint256 weight,
-        Tier tier,
-        string ipfsHash,
-        address owner
+        string tokenURI,
+        address indexed owner,
+        uint256 timestamp
     );
-
-    constructor(address marketplaceAddress) ERC721("Metaverse Tokens", "METT") {
-        contractAddress = marketplaceAddress;
+    
+    constructor(address marketplaceAddress) ERC721("Chess Test Tokens", "CHTT") Ownable(msg.sender) {
+        require(marketplaceAddress != address(0), "Invalid marketplace address");
+        _marketplaceAddress = marketplaceAddress;
     }
 
-    function createToken(
-        string memory name,
-        uint256 weight,
-        Tier tier,
-        string memory ipfsHash
-    ) public returns (uint) {
-        require(bytes(ipfsHash).length > 0, "IPFS hash cannot be empty");
-        require(bytes(name).length > 0, "Name cannot be empty");
-
-        _tokenIds++;
-        uint256 newItemId = _tokenIds;
-
-        _mint(msg.sender, newItemId);
+    function createToken(string memory tokenURI) 
+        public 
+        nonReentrant 
+        returns (uint256) 
+    {
+        require(bytes(tokenURI).length > 0, "Token URI cannot be empty");
+        require(!_usedTokenURIs[tokenURI], "Token URI already used");
         
-        // Store metadata
-        _tokenMetadata[newItemId] = NFTMetadata(
-            name,
-            weight,
-            tier,
-            ipfsHash
-        );
-
-        // Set token URI to IPFS hash
-        _setTokenURI(newItemId, ipfsHash);
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
         
-        // Approve marketplace
-        setApprovalForAll(contractAddress, true);
-
-        emit NFTMinted(newItemId, name, weight, tier, ipfsHash, msg.sender);
-        emit Transfer(address(0), msg.sender, newItemId);
-
+        _usedTokenURIs[tokenURI] = true;
+        
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        
+        if (!isApprovedForAll(msg.sender, _marketplaceAddress)) {
+            setApprovalForAll(_marketplaceAddress, true);
+        }
+        
+        emit NFTMinted(newItemId, tokenURI, msg.sender, block.timestamp);
+        
         return newItemId;
     }
 
-    // Get NFT metadata
-    function getNFTMetadata(uint256 tokenId) public view returns (
-        string memory name,
-        uint256 weight,
-        Tier tier,
-        string memory ipfsHash
-    ) {
-        require(_exists(tokenId), "NFT does not exist");
-        NFTMetadata storage metadata = _tokenMetadata[tokenId];
-        return (
-            metadata.name,
-            metadata.weight,
-            metadata.tier,
-            metadata.ipfsHash
-        );
+    function getUserTokens(address owner) 
+        public 
+        view 
+        returns (uint256[] memory tokenIds, string[] memory tokenURIs) 
+    {
+        require(owner != address(0), "Invalid owner address");
+        
+        uint256 ownerTokenCount = balanceOf(owner);
+        require(ownerTokenCount > 0, "No tokens owned by this address");
+        
+        tokenIds = new uint256[](ownerTokenCount);
+        tokenURIs = new string[](ownerTokenCount);
+        
+        uint256 tokenCount = 0;
+        uint256 maxTokenId = _tokenIds.current();
+        
+        for(uint256 id = 1; id <= maxTokenId && tokenCount < ownerTokenCount; id++) {
+            try this.ownerOf(id) returns (address tokenOwner) {
+                if (tokenOwner == owner) {
+                    try this.tokenURI(id) returns (string memory uri) {
+                        require(bytes(uri).length > 0, "Invalid token URI");
+                        
+                        tokenIds[tokenCount] = id;
+                        tokenURIs[tokenCount] = uri;
+                        tokenCount++;
+                    } catch {
+                        continue;
+                    }
+                }
+            } catch {
+                continue;
+            }
+        }
+        
+        require(tokenCount > 0, "No valid tokens found");
+        return (tokenIds, tokenURIs);
     }
+
+    function verifyTokenOwnership(uint256 tokenId, address owner) 
+        public 
+        view 
+        returns (bool) 
+    {
+        try this.ownerOf(tokenId) returns (address tokenOwner) {
+            return tokenOwner == owner;
+        } catch {
+            return false;
+        }
+    }
+
+    // function _beforeTokenTransfer(
+    //     address from,
+    //     address to,
+    //     uint256 tokenId,
+    //     uint256 batchSize
+    // ) internal override {
+    //     super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    //     require(to != address(0), "Cannot transfer to zero address");
+    // }
 }
--- contracts/src/NFTMarketplace.sol
+++ contracts/src/NFTMarketplace.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
@@ -10,7 +10,7 @@ contract NFTMarket is ReentrancyGuard {
     uint256 private _itemsSold;
 
     address payable owner;
-    uint256 listingPrice = 0.025 ether;
+    uint256 listingPrice = 0.001 ether;
 
     constructor() {
         owner = payable(msg.sender);
@@ -24,10 +24,6 @@ contract NFTMarket is ReentrancyGuard {
         address payable owner;
         uint256 price;
         bool sold;
-        // Added metadata fields
-        string name;
-        uint256 weight;
-        uint8 tier; // 0=COMMON, 1=RARE, 2=EPIC, 3=LEGENDARY
         string ipfsHash;
     }
 
@@ -41,9 +37,6 @@ contract NFTMarket is ReentrancyGuard {
         address owner,
         uint256 price,
         bool sold,
-        string name,
-        uint256 weight,
-        uint8 tier,
         string ipfsHash
     );
 
@@ -55,14 +48,10 @@ contract NFTMarket is ReentrancyGuard {
         address nftContract,
         uint256 tokenId,
         uint256 price,
-        string memory name,
-        uint256 weight,
-        uint8 tier,
         string memory ipfsHash
     ) public payable nonReentrant {
         require(price > 0, "Price must be at least 1 wei");
         require(msg.value == listingPrice, "Price must be equal to the listing price");
-        require(tier <= 3, "Invalid tier value");
 
         _itemIds++;
         uint256 itemId = _itemIds;
@@ -75,9 +64,6 @@ contract NFTMarket is ReentrancyGuard {
             payable(address(0)),
             price,
             false,
-            name,
-            weight,
-            tier,
             ipfsHash
         );
 
@@ -91,9 +77,6 @@ contract NFTMarket is ReentrancyGuard {
             address(0),
             price,
             false,
-            name,
-            weight,
-            tier,
             ipfsHash
         );
     }
@@ -113,33 +96,6 @@ contract NFTMarket is ReentrancyGuard {
         idToMarketItem[itemId].sold = true;
         _itemsSold++;
         payable(owner).transfer(listingPrice);
-    }
-
-    // Added filter functions
-    function fetchMarketItemsByTier(uint8 tier) public view returns (MarketItem[] memory) {
-        require(tier <= 3, "Invalid tier value");
-        
-        uint itemCount = _itemIds;
-        uint matchingItemCount = 0;
-        uint currentIndex = 0;
-
-        // Count matching items
-        for (uint i = 0; i < itemCount; i++) {
-            if (idToMarketItem[i + 1].tier == tier && !idToMarketItem[i + 1].sold) {
-                matchingItemCount++;
-            }
-        }
-
-        MarketItem[] memory items = new MarketItem[](matchingItemCount);
-        for (uint i = 0; i < itemCount; i++) {
-            if (idToMarketItem[i + 1].tier == tier && !idToMarketItem[i + 1].sold) {
-                uint currentId = idToMarketItem[i + 1].itemId;
-                MarketItem storage currentItem = idToMarketItem[currentId];
-                items[currentIndex] = currentItem;
-                currentIndex++;
-            }
-        }
-        return items;
     }
 
     function fetchMarketItems() public view returns (MarketItem[] memory) {
