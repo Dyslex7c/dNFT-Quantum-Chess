070743a1234088a4d78f089eeb615dab24fd56ff
Tamojit Das <tamojitdas181007@gmail.com>
2025-02-22 12:28:39 +0530

    Socket init

---
+++ client/context/SocketContext.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState, ReactNode } from "react";
+import { io, Socket } from "socket.io-client";
+import { useAccount } from "wagmi";
+
+interface SocketContextType {
+    socket: Socket | null;
+    onlinePlayers: string[];
+}
+
+interface SocketProviderProps {
+    children: ReactNode;
+}
+
+export const SocketContext = createContext<SocketContextType | undefined>(undefined);
+
+export const useSocketContext = () => {
+    const context = useContext(SocketContext);
+    if (!context) {
+        throw new Error("useSocketContext must be used within a SocketContextProvider");
+    }
+    return context;
+};
+
+export const SocketContextProvider: React.FC<SocketProviderProps> = ({ children }) => {
+    const { address } = useAccount();
+    const [socket, setSocket] = useState<Socket | null>(null);
+    const [onlinePlayers, setOnlinePlayers] = useState<string[]>([]);
+
+    useEffect(() => {
+        if (address) {
+            const newSocket = io("http://localhost:5000", {
+                query: {
+                    userId: address,
+                },
+            });
+
+            setSocket(newSocket);
+
+            newSocket.on("getOnlinePlayers", (users: string[]) => {
+                setOnlinePlayers(users);
+            });
+
+            return () => {
+                newSocket.close();
+                setSocket(null);
+            };
+        } else {
+            if (socket) {
+                socket.close();
+                setSocket(null);
+            }
+        }
+    }, [address]);
+
+    return (
+        <SocketContext.Provider value={{ socket, onlinePlayers }}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
