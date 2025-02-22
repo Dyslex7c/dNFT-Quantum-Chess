f002ffa767e720d32d3be2dafb5ff2db312f1496
Dyslex7c <polynsominiac@gmail.com>
2025-02-23 02:24:26 +0530

    proof verification done

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    // Important: Return the modified config
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      readline: false,
+      stream: false,
+    };
+    return config;
+  },
   images: {
     domains: ["aqua-past-reindeer-831.mypinata.cloud","lavender-main-tiger-401.mypinata.cloud"],
   },
